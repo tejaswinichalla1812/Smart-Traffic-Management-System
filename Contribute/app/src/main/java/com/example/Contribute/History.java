7645528c185e8bc58432d98a16138a48076a30be
ghantasumanth <89597037+ghantasumanth@users.noreply.github.com>
2022-05-31 17:37:39 -0500

    updated onclicklistener and user data

--- Contribute/app/src/main/java/com/example/Contribute/History.java
+++ Contribute/app/src/main/java/com/example/Contribute/History.java
@@ -32,3 +32,37 @@ public class History extends AppCompatActivity {
         textViewData=findViewById(R.id.data);
         loadNotes();
     }
+    public void loadNotes() {
+        notebookref.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String data="";
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                if (document.contains("name") && document.contains("description") && document.contains("user type") && document.contains("userid")) {
+
+                                    String name = (String) document.get("name");
+                                    String type = (String) document.get("user type");
+                                    String description = (String) document.get("description");
+                                    String Userid = (String) document.get("userid");
+                                    String userID = fAuth.getCurrentUser().getUid();
+                                    Timestamp ts = (Timestamp) document.get("timestamp");
+                                    String dateandtime=String.valueOf(ts.toDate());
+
+
+                                    if(Userid.equals(userID)) {
+                                        data += "Name: " + name + "\nUser Type: " + type + "\nDescription: " + description + "\nDate & Time: " + dateandtime + "\n\n";
+                                    }
+                                    textViewData.setText(data);
+                                }
+                            }
+                        }
+                        else {
+                            Log.d(TAG, "Error fetching data: ", task.getException());
+                        }
+                    }
+                });
+    }
+}
