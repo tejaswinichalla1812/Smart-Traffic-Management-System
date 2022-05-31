7379916df1f014417a56c4614b384742423e51aa
tejaswinichalla1812 <tejaswini.challa12@gmail.com>
2022-05-31 16:20:46 -0500

    Created receive.xml and receive.java files.

---
+++ Contribute/app/src/main/java/com/example/Contribute/Receive.java
@@ -0,0 +1,14 @@
+package com.example.Contribute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Receive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive);
+    }
+}
