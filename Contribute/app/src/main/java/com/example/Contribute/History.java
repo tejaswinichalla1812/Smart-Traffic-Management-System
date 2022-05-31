0ed5503f7fc3865eca1e0c67ce9ae3c0f033573c
ghantasumanth <89597037+ghantasumanth@users.noreply.github.com>
2022-05-31 17:35:59 -0500

    added code in History.java

--- Contribute/app/src/main/java/com/example/Contribute/History.java
+++ Contribute/app/src/main/java/com/example/Contribute/History.java
@@ -1,14 +1,34 @@
 package com.example.Contribute;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class History extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookref = db.collection("user data");
+    public static final String TAG = "TAG";
+    private TextView textViewData;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        fAuth= FirebaseAuth.getInstance();
+        textViewData=findViewById(R.id.data);
+        loadNotes();
     }
-}
