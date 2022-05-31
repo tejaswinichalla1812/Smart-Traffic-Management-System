1d958ff8d465ed459a637dfd827fcb1942898018
rohan2453 <rohan.r2453@gmail.com>
2022-05-31 17:05:44 -0500

    Created GoodWillLogin.java and activity_good_will_login.xml.

---
+++ Contribute/app/src/main/java/com/example/Contribute/GoodWillLogin.java
@@ -0,0 +1,14 @@
+package com.example.Contribute;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoodWillLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_will_login);
+    }
+}
