c1c7d6be64321d68830857771bfa69883c3c6706
Chakri15099 <chakradhar.prathivada1509@gmail.com>
2022-05-31 17:44:39 -0500

    Updated code in mypin page

--- Contribute/app/src/main/java/com/example/Contribute/MyPin.java
+++ Contribute/app/src/main/java/com/example/Contribute/MyPin.java
@@ -91,3 +91,75 @@ public class MyPin extends AppCompatActivity implements OnMapReadyCallback, Goog
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
         mMap.addMarker(markerOptions1).showInfoWindow();
     }
+    public void showLocation() {
+        this.cloudstorage = FirebaseFirestore.getInstance();
+        cloudstorage.collection("user data")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());//
+                                if (document.contains("location") && document.contains("name") && document.contains("description") && document.contains("userid")) {
+                                    GeoPoint location = (GeoPoint) document.get("location");
+                                    String title = (String) document.get("name");
+                                    String type = (String) document.get("type");
+                                    String description = (String) document.get("description");
+                                    String Userid = (String) document.get("userid");
+                                    String userID = fAuth.getCurrentUser().getUid();
+
+                                    if(type.equals("Donor") & Userid.equals(userID)) {
+                                        Log.d(TAG, userID + " Success " + title);
+                                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                        mMap.addMarker(new MarkerOptions().position(latLng).title(title+"("+type+")").snippet(description).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                                    }
+                                    else if(type.equals("Receiver") & Userid.equals(userID)){
+                                        Log.d(TAG, String.valueOf(location) + " Success " + title);
+                                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                        //mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                        mMap.addMarker(new MarkerOptions().position(latLng).title(title+"("+type+")").snippet(description).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                                    }
+                                }
+                            }
+                        } else {
+                            Log.d(TAG, "Error fetching data: ", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0]  == PackageManager.PERMISSION_GRANTED){
+                mapFragment.getMapAsync(this);
+            }else{
+                Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
