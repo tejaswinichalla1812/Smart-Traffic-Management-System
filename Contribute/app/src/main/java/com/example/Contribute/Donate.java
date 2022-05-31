6c6504bb364fd668e6d69408e1728e0ac422f3fe
ghantasumanth <89597037+ghantasumanth@users.noreply.github.com>
2022-05-31 17:17:26 -0500

    updated notifications and payload in donate page

--- Contribute/app/src/main/java/com/example/Contribute/Donate.java
+++ Contribute/app/src/main/java/com/example/Contribute/Donate.java
@@ -93,3 +93,130 @@ public class Donate extends AppCompatActivity {
                 String addressss = address.getText().toString();
                 String foodDurationTime = foodDuration.getText().toString();
                 String type = "Donor";
+                if (TextUtils.isEmpty(fullname)) {
+                    mFullName.setError("Name is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(addressss)) {
+                    address.setError("Required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(code)) {
+                    pincode.setError("Required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(foodDurationTime)) {
+                    foodDuration.setError("Required.");
+                    return;
+                }
+                if (TextUtils.isEmpty(thingstoDonate)) {
+                    donorthings.setError("Required.");
+                    return;
+                }
+                if (phone.length() < 10) {
+                    mPhone.setError("Phone Number Must be >=10 Characters");
+                    return;
+                }
+
+                userID = fAuth.getCurrentUser().getUid();
+                CollectionReference collectionReference = fStore.collection("user data");
+                Map<String, Object> user = new HashMap<>();
+                user.put("name", fullname);
+                user.put("food item", "");
+                user.put("phone", phone);
+                user.put("description", description);
+                user.put("pincode", code);
+                user.put("address", addressss);
+                user.put("thingstoDonate", thingstoDonate);
+                user.put("userid", userID);
+                user.put("latitude", latitude);
+                user.put("longitude", longitude);
+                user.put("isOpen", "true");
+                user.put("foodDurationTime", foodDurationTime);
+                user.put("type", type);
+
+
+                collectionReference.add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                sendNotification(fullname, thingstoDonate, type);
+                                Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, "Success!");
+                                Intent intent = new Intent(Donate.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
+                                Log.w(TAG, "Error!", e);
+                            }
+                        });
+            }
+        });
+
+        navigate_donor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //   checkPermmission
+                checkGPS();
+            }
+        });
+    }
+
+    private void sendNotification(String fullname, String fooditem, String type) {
+        String topic = "/topics/FOOD";
+        String title = "DONOR";
+        try {
+            JSONObject notification = new JSONObject();
+            JSONObject notificationBody = new JSONObject();
+            try {
+                notificationBody.put("title", title);
+                notificationBody.put("message", fullname + " wants to donate " + fooditem);
+                notification.put("to", topic);
+                notification.put("data", notificationBody);
+            } catch (Exception e) {
+                e.getMessage();
+            }
+            sendPayload(notification);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendPayload(JSONObject notification) {
+        try {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "https://fcm.googleapis.com/fcm/send";
+            JsonObjectRequest request = new JsonObjectRequest(url, notification, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Toast.makeText(Donate.this, "Notification Sent", Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(Donate.this, "Notification not sent", Toast.LENGTH_SHORT).show();
+                }
+            }) {
+                @Override
+                public Map<String, String> getHeaders() {
+                    String api_key_header_value = "Key=AAAA5fPOgyY:APA91bFNMsEmGFiVPIhelK92PPeJR0b4wiqTr7uvaHHkGFP6bAWvtrjX7kL9H7ws2pIJPzm_2SwLAGk1vNXgE-DDDbcZ81GdxxeMVuJkZ1dF0q_RMq20Et8BRsUVT7fuN6fmzROB6qd7";
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("Authorization", api_key_header_value);
+                    return headers;
+                }
+            };
+
+            queue.add(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
